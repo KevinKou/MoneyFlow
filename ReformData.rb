b94e6b3a4782565f56c40bbd9ad8e7b5e02de136
KevinKou <kevin_kou@icloud.com>
2017-01-16 18:31:04 +0900

    Update ReformData.rb

--- ReformData.rb
+++ ReformData.rb
@@ -1,8 +1,10 @@
-module ReformData
-	def ClassifyData(targetFilePath)
+require 'csv'
 
+module ReformData
+
+	def ClassifyData(targetFilePath)
 		@src_filePath   = targetFilePath
-		@destFilePath   = File.basename(targetFilePath, ".csv") + "_reformed.csv"
+		@destFilePath   = File.basename(targetFilePath, ".xls") + "_reformed.csv"
 		fileBaseName 	= File.basename(targetFilePath).upcase
 
 		if fileBaseName.include?("AMEX")
@@ -12,39 +14,42 @@ module ReformData
 		elsif fileBaseName.include?("RESONA")
 			reformData_Resona
 		end
-
-		# ofx_reformed = File.open(destFilePath,"w")
-		# ofx_reformed.puts("日付,金額,支払い先")
-		# File.foreach (srcFilePath) do |line|
-		# 	ofx_reformed.puts(line.encode("UTF-8","Shift_JIS"))
-		# end
-		# ofx_reformed.close
 	end
 
 	def reformData_AMEX
-		puts "amex"
-		puts @destFilePath
+		whichOS
+		# puts "amex"
+		# puts @destFilePath
 		dest_file = File.open(@destFilePath, "w")
 		dest_file.puts("口座,説明,受取人,カテゴリ,日付,金額,タグ,")
-		account = "AMEX"
+
+		account   = "AMEX"
 		File.foreach (@src_filePath) do |line|
-			line_utf8 = line.encode("UTF-8","Shift_JIS").split(",")
-			descript = ""
-			payto  = line_utf8[2]
-			kategorie = payto	# <- ここは判定する箇所いる
-			data = line_utf8[0]
-			price = line_utf8[1]
-			tag = payto # <- ここは判定する箇所いる
-			line_str = account + "," + descript + "," + payto + "," + kategorie + "," + data + "," + price + "," + tag + ","
- 			dest_file.puts(line_str)
+			if @os == :windows
+				line_utf8 = line.split("	")
+			elsif @os == :macosx
+				line_utf8 = line.encode("UTF-8","Shift_JIS").split("	")
+			end
+
+			payto      = line_utf8[2].strip
+			retrun_str = doClassify(payto) # <- ここは判定する箇所いる
+			kategorie  = retrun_str[0]
+			tag        = retrun_str[1]
+			descript   = retrun_str[2]
+			if descript == "skip"
+				next
+			end
+			data       = line_utf8[0].strip
+			price      = "-" + line_utf8[1].gsub("\"","").gsub(",","").strip
+			line_str   = account + "," + descript + "," + payto + "," + kategorie + "," + data + "," + price + "," + tag + ","
+			dest_file.puts(line_str)
 		end
 		dest_file.close
-		
 	end
 
 	def reformData_UFJ
 		puts "ufj"
-		puts @src_filePath	
+		puts @src_filePath
 	end
 
 	def reformData_Resona
@@ -52,8 +57,51 @@ module ReformData
 		puts @src_filePath
 	end
 
-	module_function :ClassifyData	# <- これがないと外部から呼び出せない
+	def whichOS
+		@os ||= (
+			host_os = RbConfig::CONFIG['host_os']
+			case host_os
+			when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+			  :windows
+			when /darwin|mac os/
+			  :macosx
+			when /linux/
+			  :linux
+			when /solaris|bsd/
+			  :unix
+			else
+			  :unknown
+			end
+		)
+	end
+
+	def doClassify(paytowho)
+		kategorie_str = ""
+		tag_str       = ""
+		description   = ""
+
+		define_info = CSV.read("./Classify_define.csv", headers: true)
+		define_info.each {
+			|define_data|
+			if paytowho.include?(define_data["PayToWho"])
+				if define_data["Kategorie"] != nil
+					kategorie_str = define_data["Kategorie"]
+				end
+				if define_data["Tag"] != nil
+					tag_str       = define_data["Tag"]
+				end
+				if define_data["Description"] != nil
+					description   = define_data["Description"]
+				end
+			end
+		}
+		return kategorie_str, tag_str, description
+	end
+
+	module_function :ClassifyData	# <- これがない呼び出せない
 	module_function :reformData_AMEX
 	module_function :reformData_UFJ
 	module_function :reformData_Resona
+	module_function :whichOS
+	module_function :doClassify
 end
